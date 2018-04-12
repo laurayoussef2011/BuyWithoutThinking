50e8c01604c381010a2399ba89702610c022fd7b
Laura Youssef <laurayoussef2011@gmail.com>
2018-04-13 01:16:53 +0200

    sprint#4 with Angular

---
+++ src/main/java/com/SWEProject/repository/HistoryRepository.java
@@ -0,0 +1,7 @@
+package com.SWEProject.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import com.SWEProject.Entities.History;
+
+public interface HistoryRepository extends CrudRepository< History, Integer>{
+}
