2f26e4e155f1d422cfaef9ec8c5b16d5af0ec482
fady <fadymbt@gmail.com>
2018-04-28 23:40:06 +0200

    Product service test using mockito

--- src/main/java/TestNG/TestNGProductService/ProductServiceTest.java
+++ src/main/java/TestNG/TestNGProductService/ProductServiceTest.java
@@ -3,300 +3,152 @@ package TestNG.TestNGProductService;
 import com.SWEProject.Entities.Product;
 import com.SWEProject.Entities.SystemProduct;
 import com.SWEProject.Entities.User;
+import com.SWEProject.repository.BrandRepository;
 import com.SWEProject.repository.ProductRepository;
+import com.SWEProject.repository.SystemProductRepository;
+import com.SWEProject.repository.UserRepository;
+import com.SWEProject.service.BrandService;
 import com.SWEProject.service.ProductService;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.Mockito.when;
 import static org.testng.Assert.*;
 
 public class ProductServiceTest {
 
-    ProductService productService = new ProductService();
-
-    @Autowired
+    @Mock
     ProductRepository productRep;
+    @Mock
+    SystemProductRepository systemProductRep;
+    @Mock
+    UserRepository userRep;
 
-    @DataProvider(name = "systemProductCheckAvailableSuccess")
-    public static Object[][] Test_systemProductCheckAvailableSuccess()
-    {
-        return new Object[][]{
-                {"iphone", "iphone", "apple", 5.0},
-                {"galaxy", "phone", "samsung", 5.0},
-                {"335", "phone", "nokia", 5.0}
-        };
-    }
-    @DataProvider(name = "systemProductCheckAvailableFailure")
-    public static Object[][] Test_systemProductCheckAvailableFailure()
-    {
-        return new Object[][]{
-                {"ggsgs", "fffaef", "fafafa", 1100000000},
-                {"qwer", "asd", "adf", 50000},
-                {"qwerty", "zxc", "ikl", 7895462}
-        };
-    }
-    @DataProvider(name = "systemProductCheckAvailableError")
-    public static Object[][] Test_systemProductCheckAvailableError()
-    {
-        return new Object[][]{
-                {" "," "," ",0}
-        };
+    @InjectMocks
+    ProductService productService;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
     }
 
 
+    @Test
+    public void testSystemProductCheckAvailableSuccess()
+    {
+        String productName = "iPhone";
+        String model = "4s";
+        String brand = "Apple";
+        double price = 1000;
+        SystemProduct systemProduct = new SystemProduct(productName, model, brand, price);
 
-    @DataProvider(name = "addProductSuccess")
-    public static Object[][] Test_addProductSuccess()
-    {
-        return new Object[][]{
-                {"iphone","tradeline",130000,"phone","apple",5,100,true},
-                {"galaxy","samsungstore",130000,"phone","samsung",6,200,true}
-        };
-    }
-    @DataProvider(name = "addProductFailure")
-    public static Object[][] Test_addProductFailure()
-    {
-        return new Object[][]{
-                {"s","s",1,"s","s",0,1000,true},
-                {"f","f",96,"p","sams",7,2500,false}
-        };
-    }
-    @DataProvider(name = "addProductError")
-    public static Object[][] Test_addProductError()
-    {
-        return new Object[][]{
-                {" "," ",0,"","",0,0,false}
-        };
+        when(systemProductRep.save(systemProduct)).thenReturn(systemProduct);
+        Assert.assertEquals(true, productService.systemProductCheckAvailable(systemProduct));
     }
 
 
+    @Test
+    public void testSystemProductCheckAvailableFailure()
+    {
+        String productName = "iPhone";
+        String model = "4s";
+        String brand = "Apple";
+        double price = 1000;
+        SystemProduct systemProduct = new SystemProduct(productName, model, brand, price);
 
-    @DataProvider(name = "getProductInfoSuccess")
-    public static Object[][] TestgetProductInfoSuccess()
-    {
-        return new Object[][]{
-                {1},
-                {2},
-                {3}
-        };
-    }
-    @DataProvider(name = "getProductInfoFailure")
-    public static Object[][] TestgetProductInfoFailure()
-    {
-        return new Object[][]{
-                {100},
-                {200},
-                {300}
-        };
-    }
-    @DataProvider(name = "getProductInfoError")
-    public static Object[][] TestgetProductInfoError()
-    {
-        return new Object[][]{
-                {-1},
-                {-2},
-                {-3}
-        };
+        when(systemProductRep.save(systemProduct)).thenReturn(null);
+        Assert.assertEquals(false, productService.systemProductCheckAvailable(systemProduct));
     }
 
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+    @Test
+    public void AddProductSuccess()
+    {
+        String productName = "iPhone";
+        String storeName = "tradeline";
+        Integer price = 1000;
+        String model = "4s";
+        String brand = "Apple";
+        Integer views = 0;
+        Integer quantity = 10;
+        boolean available = true;
 
-    @DataProvider(name = "getStoreProductListSuccess")
-    public static Object[][] TestgetStoreProductListSuccess()
-    {
-        return new Object[][]{
-                {"nike"},
-                {"addidas"},
-                {"tradeline"}
-        };
+        Product product = new Product(productName, storeName, price, model, brand, views, quantity, available);
+        when(productRep.save(product)).thenReturn(product);
+        Assert.assertEquals(true, productService.addProduct(product));
     }
-    @DataProvider(name = "getStoreProductListFailure")
-    public static Object[][] TestgetStoreProductListFailure()
+    @Test
+    public void AddProductFailure()
     {
-        return new Object[][]{
-                {"h&m"},
-                {"active_abo_alaa"},
-                {"Hardees"}
-        };
-    }
-    @DataProvider(name = "getStoreProductListError")
-    public static Object[][] TestgetStoreProductListError()
-    {
-        return new Object[][]{
-                {""},
-                {" "},
-        };
-    }
+        String productName = "iPhone";
+        String storeName = "tradeline";
+        Integer price = 1000;
+        String model = "4s";
+        String brand = "Apple";
+        Integer views = 0;
+        Integer quantity = 10;
+        boolean available = true;
 
-
-
-    @DataProvider(name = "buyProductSuccess")
-    public static Object[][] TestbuyProductSuccess()
-    {
-        return new Object[][]{
-                {1,"haram street",2, "m.samy", 500},
-                {1,"faisal street", 3, "shaar", 600}
-        };
-    }
-    @DataProvider(name = "buyProductFailure")
-    public static Object[][] TestbuyProductFailure()
-    {
-        return new Object[][]{
-                {1,"haram street",2, "Odin", 500},
-                {1,"faisal street", 3, "What", 600}
-        };
-    }
-    @DataProvider(name = "buyProductError")
-    public static Object[][] TestbuyProductError()
-    {
-        return new Object[][]{
-                {-9," ",-9, " ", -9},
-                {-9," ", 3, " ", -9}
-        };
+        Product product = new Product(productName, storeName, price, model, brand, views, quantity, available);
+        when(productRep.save(product)).thenReturn(null);
+        Assert.assertEquals(false, productService.addProduct(product));
     }
 
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+    @Test
+    public void GetProductInfoSuccess()
+    {
+        Integer serialNumber = 1;
 
-    @DataProvider(name = "removeProductSuccess")
-    public static Object[][] TestremoveProduct()
-    {
-        return new Object[][]{
-                {1},
-                {2},
-                {3}
-        };
-    }
-    @DataProvider(name = "removeProductFailure")
-    public static Object[][] TestremoveProductFailure()
-    {
-        return new Object[][]{
-                {100},
-                {200},
-                {300}
-        };
-    }
-    @DataProvider(name = "removeProductError")
-    public static Object[][] TestremoveProductError()
-    {
-        return new Object[][]{
-                {-1},
-                {-2},
-                {-3}
-        };
+        Product product = new Product();
+        product.setSerialnumber(serialNumber);
+        when(productRep.findOne(serialNumber)).thenReturn(product);
+        Assert.assertEquals(product, productService.getProductInfo(serialNumber));
     }
 
+    @Test
+    public void GetProductInfoFailure()
+    {
+        Integer serialNumber = 100; // wrong serial number
 
-
-    @Test(dataProvider = "systemProductCheckAvailableSuccess")
-    public void testSystemProductCheckAvailableSuccess(String productName,String Model, String Brand, double Price)
-    {
-        SystemProduct tempsystemProduct = new SystemProduct(productName, Model, Brand, Price);
-        Assert.assertEquals(true, productService.systemProductCheckAvailable(tempsystemProduct));
-    }
-    @Test(dataProvider = "systemProductCheckAvailableFailure")
-    public void testSystemProductCheckAvailableFailure(String productName,String Model, String Brand, double Price)
-    {
-        SystemProduct tempsystemProduct = new SystemProduct(productName, Model, Brand, Price);
-        Assert.assertEquals(false, productService.systemProductCheckAvailable(tempsystemProduct));
-    }
-    @Test(dataProvider = "systemProductCheckAvailableError")
-    public void testSystemProductCheckAvailableError(String productName,String Model, String Brand, double Price)
-    {
-        SystemProduct tempsystemProduct = new SystemProduct(productName, Model, Brand, Price);
-        Assert.assertEquals(false, productService.systemProductCheckAvailable(tempsystemProduct));
+        Product product = new Product();
+        product.setSerialnumber(serialNumber);
+        when(productRep.findOne(serialNumber)).thenReturn(null);
+        Assert.assertEquals(null, productService.getProductInfo(serialNumber));
     }
 
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-    @Test(dataProvider = "addProductSuccess")
-    public void testAddProductSuccess(String productName, String storeName, Integer price, String model, String brand, Integer views ,Integer quantity, boolean available)
+    @Test
+    public void GetStoreProductListSuccess()
     {
-        Product tempProduct = new Product(productName, storeName, price, model, brand, views, quantity, available);
-        Assert.assertEquals(true, productService.addProduct(tempProduct));
-    }
-    @Test(dataProvider = "addProductFailure")
-    public void testAddProductFailure(String productName, String storeName, Integer price, String model, String brand, Integer views ,Integer quantity, boolean available)
-    {
-        Product tempProduct = new Product(productName, storeName, price, model, brand, views, quantity, available);
-        Assert.assertEquals(false, productService.addProduct(tempProduct));
-    }
-    @Test(dataProvider = "addProductError")
-    public void testAddProductError(String productName, String storeName, Integer price, String model, String brand, Integer views ,Integer quantity, boolean available)
-    {
-        Product tempProduct = new Product(productName, storeName, price, model, brand, views, quantity, available);
-        Assert.assertEquals(false, productService.addProduct(tempProduct));
-    }
-
-
-
-    @Test(dataProvider = "getProductInfoSuccess")
-    public void testGetProductInfoSuccess(Integer serialNumber)
-    {
-        Product tempProduct = productRep.findOne(serialNumber);
-        Assert.assertEquals(tempProduct, productService.getProductInfo(serialNumber));
-    }
-    @Test(dataProvider = "getProductInfoFailure")
-    public void testGetProductInfoFailure(Integer serialNumber)
-    {
-        Product tempProduct = productRep.findOne(serialNumber);
-        Assert.assertEquals(tempProduct, productService.getProductInfo(serialNumber));
-    }
-    @Test(dataProvider = "getProductInfoError")
-    public void testGetProductInfoError(Integer serialNumber)
-    {
-        Product tempProduct = productRep.findOne(serialNumber);
-        Assert.assertEquals(tempProduct, productService.getProductInfo(serialNumber));
-    }
-
-
-
-    @Test(dataProvider = "getStoreProductListSuccess")
-    public void testGetStoreProductListSuccess(String storeName)
-    {
-        Iterable<Product> productIterable= productRep.findAll();
-        List<Product> productList =new ArrayList<Product>();
-        for(Product product : productIterable)
-        {
-            if(product.getStorename().equals(storeName) && product.getAvailable())
-            {
-                productList.add(product);
-            }
-        }
+        String storeName = "tradeline";
+        List<Product> productList = new ArrayList();
+        when(productRep.findAll()).thenReturn(productList);
         Assert.assertEquals(productList, productService.getStoreProductList(storeName));
     }
-    @Test(dataProvider = "getStoreProductListFailure")
-    public void testGetStoreProductListFailure(String storeName)
+    @Test
+    public void GetStoreProductListFailure()
     {
-        Iterable<Product> productIterable= productRep.findAll();
-        List<Product> productList =new ArrayList<Product>();
-        for(Product product : productIterable)
-        {
-            if(product.getStorename().equals(storeName) && product.getAvailable())
-            {
-                productList.add(product);
-            }
-        }
-        Assert.assertEquals(productList, productService.getStoreProductList(storeName));
+        String storeName = "tradeline";
+        List<Product> productList = new ArrayList();
+        when(productRep.findAll()).thenReturn(null);
+        Assert.assertEquals(null, productService.getStoreProductList(storeName));
     }
-    @Test(dataProvider = "getStoreProductListError")
-    public void testGetStoreProductListError(String storeName)
-    {
-        Iterable<Product> productIterable= productRep.findAll();
-        List<Product> productList =new ArrayList<Product>();
-        for(Product product : productIterable)
-        {
-            if(product.getStorename().equals(storeName) && product.getAvailable())
-            {
-                productList.add(product);
-            }
-        }
-        Assert.assertEquals(productList, productService.getStoreProductList(storeName));
-    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     @Test
     public void testGetProductList()
@@ -306,53 +158,60 @@ public class ProductServiceTest {
         Assert.assertEquals(tempProductList, productService.getProductList());
     }
 
-    @Test(dataProvider = "buyProductSuccess")
-    public void testBuyProduct(Integer serialNumber ,String address , Integer quantity, String username, Integer credit)
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Test
+    public void testBuyProduct()
     {
-        User tempuser = new User();
-        tempuser.setUsername(username);
-        tempuser.setCredit(credit);
-        Assert.assertEquals(true, productService.buyProduct(serialNumber, address, quantity, tempuser));
+        Integer serialNumber = 1;
+        String address = "Dokki";
+        Integer quantity = 2;
+
+        User user = new User("laura" , "laura","laura","storeOwner","laura","",100000);
+        Product product = new Product("iPhone","", 100, "4s", "Apple",10,10,true);
+        when(productRep.findOne(serialNumber)).thenReturn(product);
+        when(userRep.findOne(user.getUsername())).thenReturn(user);
+        when(userRep.save(user)).thenReturn(user);
+        when(productRep.save(product)).thenReturn(product);
+        Assert.assertEquals(true, productService.buyProduct(serialNumber, address, quantity, user));
     }
-    @Test(dataProvider = "buyProductFailure")
-    public void testBuyProductFailure(Integer serialNumber ,String address , Integer quantity, String username, Integer credit)
+    @Test
+    public void testBuyProductFailure()
     {
-        User tempuser = new User();
-        tempuser.setUsername(username);
-        tempuser.setCredit(credit);
-        Assert.assertEquals(false, productService.buyProduct(serialNumber, address, quantity, tempuser));
-    }
-    @Test(dataProvider = "buyProductError")
-    public void testBuyProductError(Integer serialNumber ,String address , Integer quantity, String username, Integer credit)
-    {
-        User tempuser = new User();
-        tempuser.setUsername(username);
-        tempuser.setCredit(credit);
-        Assert.assertEquals(false, productService.buyProduct(serialNumber, address, quantity, tempuser));
+        Integer serialNumber = 1;
+        String address = "Dokki";
+        Integer quantity = 2;
+
+        User user = new User("laura" , "laura","laura","storeOwner","laura","",100000);
+        Product product = new Product("iPhone","", 100, "4s", "Apple",10,1,true);
+        when(productRep.findOne(serialNumber)).thenReturn(product);
+        when(userRep.findOne(user.getUsername())).thenReturn(user);
+        when(userRep.save(user)).thenReturn(user);
+        when(productRep.save(product)).thenReturn(product);
+        Assert.assertEquals(false, productService.buyProduct(serialNumber, address, quantity, user));
     }
 
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+    @Test
+    public void RemoveProductSuccess()
+    {
+        Integer serialNumber = 1;
+        Product product = new Product("iPhone","", 100, "4s", "Apple",10,1,true);
+        product.setSerialnumber(1);
+        when(productRep.exists(product.getSerialnumber())).thenReturn(true);
+        when(productRep.findOne(serialNumber)).thenReturn(product);
+        when(productRep.save(product)).thenReturn(product);
+        Assert.assertEquals(true, productService.removeProduct(product));
+    }
+    @Test
+    public void RemoveProductFailure()
+    {
+        Product product = new Product("iPhone","", 100, "4s", "Apple",10,1,true);
+        product.setSerialnumber(1);
+        when(productRep.exists(product.getSerialnumber())).thenReturn(false);
 
-    @Test(dataProvider = "removeProductSuccess")
-    public void testRemoveProduct(Integer serialNumber)
-    {
-        Product tempProduct = new Product();
-        tempProduct.setSerialnumber(serialNumber);
-        Assert.assertEquals(true, productService.removeProduct(tempProduct));
-    }
-    @Test(dataProvider = "removeProductFailure")
-    public void testRemoveProductFailure(Integer serialNumber)
-    {
-        Product tempProduct = new Product();
-        tempProduct.setSerialnumber(serialNumber);
-        Assert.assertEquals(false, productService.removeProduct(tempProduct));
-    }
-    @Test(dataProvider = "removeProductError")
-    public void testRemoveProductError(Integer serialNumber)
-    {
-        Product tempProduct = new Product();
-        tempProduct.setSerialnumber(serialNumber);
-        Assert.assertEquals(false, productService.removeProduct(tempProduct));
+        Assert.assertEquals(false, productService.removeProduct(product));
     }
 
 }
